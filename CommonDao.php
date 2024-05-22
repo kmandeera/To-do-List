3aced67d01514f4be23158fe90324c83111987b6
kmandeera <134777753+kmandeera@users.noreply.github.com>
2024-05-22 12:50:34 +0530

    Update CommonDao.php

--- CommonDao.php
+++ CommonDao.php
@@ -9,7 +9,6 @@ try{
     $conn= new PDO("mysql:host=$servername;dbname=$dbname",
     $username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    echo "Connected";
 }catch (PDOException $e){
     echo "Connection Failed :". $e->getMessage();
 }
